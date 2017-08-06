33cccb4e97756fd000ea4517dc93984335718df2
韩兆赟 <758858816@qq.com>
2017-08-06 19:36:22 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,56 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
 var git = require('gulp-git');
 var gulpSequence = require('gulp-sequence')
 
-//// Run git commit
+//RUN npm run build cmd  打包命令
+gulp.task('build', function (cb) {
+  exec('npm run build', function (err, stdout, stderr) {
+    cb(err);
+  });
+});
+//RUN cd进到子目录 child
+//**********************
+var child='admin'  //设置子目录变量名
+gulp.task('child', function (cb) {
+  exec('cd '+child, function (err, stdout, stderr) {
+    cb(err);
+  });
+});
+
+//Run git add  注意目录结构调整！！！
+gulp.task('add', function(){
+  return gulp.src(['./dist/*.*','./src/*','./*.*'])
+    .pipe(git.add());
+});
+
+//// Run git commit    注意目录结构调整！！！
 gulp.task('commit', function(){
-    return gulp.src(['./*','!./node_modules'])
-        .pipe(git.commit('ordinary commit'));
+  return gulp.src(['./dist/*.*','./src/*','./*.*'])
+    .pipe(git.commit('ordinary commit'));
+  //return gulp.src(['./*.*','./*/'])
+  //    .pipe(git.commit('ordinary commit'));
+
 });
 
 //// Run git push
-gulp.task('push', function(){
-    git.push('origin', 'master', function (err) {
-        if (err) throw err;
-    });
+gulp.task('runpush', function(){
+  git.push('origin', 'master', function (err) {
+    if (err) throw err;
+  });
 });
 
 //// Run git pull
 gulp.task('pull', function(){
-    git.pull('origin', 'master', {args: '--rebase'}, function (err) {
-        if (err) throw err;
-    });
+  git.pull('origin', 'master', {args: '--rebase'}, function (err) {
+    if (err) throw err;
+  });
 });
 
-gulp.task( 'default', gulpSequence( 'commit', 'push'));
+
+//  默认  gulp 命令执行build打包，并且推送到仓库
+gulp.task( 'default', gulpSequence( 'build','add','commit', 'runpush'));
+//  默认  gulp childbuild 命令执行子目录build打包，并且推送到仓库，子目录名在上面child变量中设置
+gulp.task( 'childbuild', gulpSequence( 'child','build','commit', 'runpush'));
+//    gulp push 命令执行推送到仓库,不执行打包
+gulp.task( 'push', gulpSequence( 'commit', 'runpush'));
